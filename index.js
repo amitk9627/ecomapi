9071177e9bdd96f9d084fba16caf7342a4da46d4
Amit kumar <ramitgzp56@gmail.com>
2023-10-02 23:25:08 +0530

    env mongodb connect

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app=require('./app.js');
 const mongoose=require('mongoose');
-
+require('dotenv').config();
 
 const mongoDB= async ()=>{
-   await mongoose.connect("mongodb://localhost:27017/backend");
+   await mongoose.connect(process.env.mongoDBurl);
 }
 mongoDB().then(()=>console.log("DB connected")).catch(()=>console.log("DB disconnected"));
 app.listen(3001,()=>{
